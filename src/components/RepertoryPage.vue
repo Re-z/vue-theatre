508e241066e9d27aee4ec578937009f6e3a327d6
re-z <re-z@ukr.net>
2019-10-01 02:19:45 +0300

    repertory page in progress

--- src/components/RepertoryPage.vue
+++ src/components/RepertoryPage.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <app-header></app-header>
-
+        <!-- todo -->
+        <!-- сделать активное состояние кнопки в списке -->
+        <!-- динамически подгружать первый элемент -->
         <main class="content">
             <section class="intro-line">
             <div class="inner">
@@ -17,7 +19,10 @@
                             <ul class="">
                                 <li 
                                     v-for="(item,index) in db" :key="index" 
-                                    v-if="item.forAge === 'young'">
+                                    v-if="item.forAge === 'young'"
+                                    @click="selectActiveShow"
+                                    :class="'is-active'"
+                                >
                                     {{ item.name }}
                                 </li>
                             </ul>
@@ -42,22 +47,22 @@
                                 </li>
                             </ul>
                         </aside>
+                        
                         <div class="block__content rep__content">
-
-                            <article id="adv" class="">
-                                <h4>Новорічні пригоди</h4>
-                                <div><img src="img/photo/rep/advantures.JPG" title="Фото">
-                                    <p>Авторська постановка, яка має на меті звернути увагу дітлахів на питання необхідності добре вчитися. Окремо висвітлюється тема самонавчання, як необхідної складової для досягнення успіху. Режисер - Бачинська Н.М.</p>
+                            <!-- Контент будет изменяться в зависимости от выбранного представления -->
+                            <article>
+                                <h4>{{selectedShow.name}}</h4>
+                                <div>
+                                    <img
+                                        v-if="selectedShow.img"
+                                        :src="require(`../assets/img/photo/rep/${selectedShow.img}`)"
+                                     >
+                                    <p>{{ selectedShow.text }}</p>
                                 </div>
+                                <!-- :src="require(`@/assets/img/photo/gallery/gallery(${img}).jpg`)" -->
                             </article>
 
-                            <article id="lyana" class="">
-                                <h4 class="is-active">Як у Ляни на галявині</h4>
-                                <div class="is-visible">
-                                    <!---->
-                                    <p>Повчально-ігрова постановка для дітей віком до 13 років. Завдяки своїй універсальності може використовуватися як повчальний спектакль-гра з будь-якою тематикою, цікавою як малятам так і дітям більш старшого віку.</p>
-                                </div>
-                            </article>
+                            
                       
                         </div>
                     </div>
@@ -67,6 +72,7 @@
         </main>
 
     <app-footer></app-footer>
+
 </div>
 </template>
 
@@ -74,30 +80,43 @@
 export default {
     data() {
         return {
-            db: '',
-            showsForYoung: ''
+            
+            db: '', //сюда будет записан объект, который прийдет с firebase и на основании него будет отрисован список представлений в DOM
+            selectedShow: { //здесь будет хранится объект активного выступления, которое в данный момент на экране
+                "name": "Навчання Коськи",
+                "img": "koska.jpg",
+                "text": "Авторська постановка на одну дію для дітей віком від 4 до 12 років, мова українська або російська, тривалість 40-45 хвилин, режисер постановки – Бачинська Н.М. В ході постановки розкриваються такі питання повсякденної життєдіяльності як правила дорожнього руху та пожежна безпека.",
+                "forAge": "young"
+            } 
         }
     },
     methods: {
-        // filterShows(filter) {
-        //     let newFilteredArray = db.filter(el => {
-        //         return el.forAge === filter
-        //     });
-        //     this.showsForYoung = newFilteredArray;
-        //     console.log(showsForYoung);
-        // }
+        
+        selectActiveShow(event) {
+            //забираем текст с кликнувшего элемента. Фильтруем на его основе весь масив и получаем массив с одним, активным объектом (на который кликнули)
+            let selectedName = event.target.textContent.trim();
+            let selectedShowObject = this.db.filter(el => {
+                if(el.name === selectedName) {
+                    return {el}
+                }
+            });
+            console.log(this.selectedShow);
+            //далее присваиваем значения этого объекта в переменную selectedShow, на основании которой отрисовываются элементы DOM
+           this.selectedShow = selectedShowObject[0]
+        }
         
     },
     created() {
+        //забираем данные с firebase
         async function getDataFromDB() {
             let response = await fetch('https://vp-teatr.firebaseio.com/repertory.json');
             let data = await response.json();
             return data;
         };
+        // получаем промис
         let data = getDataFromDB()
+        // когда данные будут получены - они присвоятся переменной db в data
             .then(a => this.db = a);
-        // console.log('before create');
-        // this.filterShows('young')
     },
     
 }
